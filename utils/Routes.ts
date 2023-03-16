f8093aa457f91ff848d2ebf66440014c7404e689
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-03-16 19:07:08 +0200

    added auth-redirect page

--- utils/Routes.ts
+++ utils/Routes.ts
@@ -1,7 +1,7 @@
 //create a singleton class for routes
 export class Routes {
   //create an enum with all the routes
-  public static readonly REDIRECT_FROM_AUTH = '/from-auth/:token';
+  public static readonly REDIRECT_FROM_AUTH = '/auth-redirect/:token';
   public static readonly HOME = '/';
   public static readonly SCHEDULE = '/schedule';
   public static readonly APPOINTMENTS = '/appointments';
