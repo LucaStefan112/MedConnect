49aef0d24e1368e7befe8335f9bf63f51d7c7fc9
Birladeanu Rares <birladeanurares@gmail.com>
2023-03-16 18:24:58 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{jsx,js,ts,tsx}', './components/**/*.{jsx,js,ts,tsx}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      blue: {
+        100: '#dbeafe',
+        200: '#bfdbfe',
+        300: '#93c5fd',
+        400: '#60a5fa',
+        500: '#3b82f6',
+        600: '#2563eb',
+        700: '#1d4ed8',
+        800: '#1e40af',
+        900: '#1e3a8a',
+      },
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      boxShadow: {
+        'outline-blue': '0 0 0 3px rgba(59, 130, 246, 0.5)',
+      },
+  
+    }
+  }
 }
