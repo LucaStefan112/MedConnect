7903ac701a28f2e47a6aff135180ee7cc1af2fe3
Birladeanu Rares <birladeanurares@gmail.com>
2023-03-19 15:57:59 +0200

    created MUI theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,36 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router';
 import path from 'path';
+import { createTheme , colors , ThemeProvider } from '@mui/material';
 
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname }: { pathname: string } = useRouter();
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: colors.blue[500],
+    },
+    secondary: {
+      main: colors.blue[500],
+    },
+  },
+  typography: {
+    fontFamily: 'Inner',
+    fontWeightLight: 100,
+    fontWeightRegular: 200,
+    fontWeightMedium: 300,
+    fontWeightBold: 400,
+
+  },
+});
+
   return (
+    <ThemeProvider theme={theme}>
     <div className='page'>
       { !pathname.includes('auth-redirect') && <SideMenu /> }
       <Component {...pageProps} />
     </div>
+    </ThemeProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import { Routes } from '@/utils/Routes'
 import { Button, Link } from '@mui/material'
 import React from 'react'
+import { createTheme , colors , ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: colors.blue[500],
+    },
+    secondary: {
+      main: colors.blue[500],
+    },
+  },
+  typography: {
+    fontFamily: 'Inner',
+    fontWeightLight: 100,
+    fontWeightRegular: 200,
+    fontWeightMedium: 300,
+    fontWeightBold: 400,
+
+  },
+});
 
 export default function Home() {
   return (
+    <ThemeProvider theme={theme}>
     <div className='flex flex-row justify-between items-center'>
       <h1 className='text-2xl font-bold'>Home</h1>
       <Link href={ Routes.SCHEDULE } className='
@@ -18,5 +39,6 @@ export default function Home() {
         </Button>
       </Link>
     </div>
+    </ThemeProvider>
   )
 }
