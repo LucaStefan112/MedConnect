ce79f4009becc8324942057af4f1ee443d4ac7eb
Tudor Gălățan <tudorgalatan@gmail.com>
2023-03-21 09:41:42 +0200

    Updated My Account Body Component
    I have updated the My Account Body component.

--- components/MyAccount/MyAccountBody.tsx
+++ components/MyAccount/MyAccountBody.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyAccountForm from '@/components/MyAccount/MyAccountForm';
 
 
 
@@ -6,9 +7,11 @@ export default function MyAccountBody ()
 {
   return (
 
-    <div>
+    <div className = 'flex flex-row items-center justify-between'>
 
-      Body
+      <MyAccountForm />
+      <MyAccountForm />
+      <MyAccountForm />
     
     </div>
 
