21a3e0981b697776a82c387dd8ac2188c20b3fdd
Tudor Gălățan <tudorgalatan@gmail.com>
2023-03-21 09:35:55 +0200

    Updated My Account Header Height
    I have updated the height of the My Account Header component.

--- components/MyAccount/MyAccountHeader.tsx
+++ components/MyAccount/MyAccountHeader.tsx
@@ -11,7 +11,7 @@ export default function MyAccountHeader ()
     <div className = 'flex flex-row items-center justify-between mb-10 border-b-8 border-blue-500' >
 
         <PersonIcon className ='
-            h-36 w-36
+            h-24 w-24
             bg-blue-50
             hover:bg-blue-100
             rounded-xl
@@ -20,7 +20,7 @@ export default function MyAccountHeader ()
             color = 'primary'
         />
 
-        <Button className = 'main_button h-12 w-28 text-lg'>Save</Button>
+        <Button className = 'main_button h-10 w-20 text-lg'>Save</Button>
 
     </div>
 
