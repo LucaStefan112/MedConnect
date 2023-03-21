214862746621cec8047ae92dce02fbec730ba0cf
rusuioana7 <rusuioana37@gmail.com>
2023-03-21 09:51:36 +0200

    icons folder
    all the icons and pictures in a folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Image from "next/image"
 import SideMenu from '../components/SideMenu/SideMenu'
+import userbox from '../public/icons-folder/User_box_duotone.png'
 
 export default function Home() {
   return (
-    <div>home</div>
+    <div>
+      <Image src={userbox} alt="user"></Image>
+    </div>
   )
 }
