d3f75c80c5658d2f62a818bbdc097b0a2c777a62
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-03-22 10:45:13 +0200

    small  changes to display

--- pages/my-account.tsx
+++ pages/my-account.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import MyAccountHeader from '@/components/MyAccount/MyAccountHeader';
-import MyAccountBody from '@/components/MyAccount/MyAccountBody';
 import UserForm from '@/components/UserForm/UserForm';
+import PersonIcon from '@mui/icons-material/Person';
+import { Button } from '@mui/material';
 
 export default function Profile() {
   return (
-    <div className = 'main_page'>
-      <div className = 'w-full ml-20 mr-20'>
-        <MyAccountHeader />
-        <MyAccountBody />
+    <div className='main_page'>
+      <div className='
+        w-89_percent flex flex-row items-center justify-between 
+        mb-10 border-b-8 border-blue-500'
+      >
+        <PersonIcon className ='
+            h-24 w-24
+            bg-blue-50
+            hover:bg-blue-100
+            rounded-xl
+            drop-shadow-md
+            mb-6'
+            color = 'primary'
+        />
+        <Button className = 'main_button h-10 w-20 text-lg'>Save</Button>
       </div>
+      <UserForm />
     </div>
   )
 }
