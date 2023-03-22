1045bd79bf8816901ce8ccf60a6852e024e9acb9
Luca-Stefan Tamas <lucastefantamas@gmail.com>
2023-03-22 11:06:37 +0200

    Update Routes.ts

--- utils/Routes.ts
+++ utils/Routes.ts
@@ -7,7 +7,7 @@ export default class Routes {
   public static APPOINTMENTS = '/appointments';
   public static APPOINTMENT = (id: any) => `/appointments/${id}`;
   public static ANALYSES = '/analyses';
-  public static ANALYSIS = '/analysis/:id';
+  public static ANALYSIS = '/analyses/:id';
   public static MY_ACCOUNT = '/my-account';
   public static SIGNUP = '/signup';
   public static SIGNIN = '/signin';
