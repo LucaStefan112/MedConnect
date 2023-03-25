5026f2f547a9659a2f038b69adb7d5198e3d40ba
Tudor Gălățan <tudorgalatan@gmail.com>
2023-03-25 13:14:39 +0200

    Updated Upcoming Appointments
    I have updated the Upcoming Appointments.

--- components/UpcomingAppointments/UpcomingAppointments.tsx
+++ components/UpcomingAppointments/UpcomingAppointments.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { Button } from '@mui/material'
 import Link from 'next/link'
 import Routes from '@/utils/Routes'
+import Appointment from '@/pages/appointments/[id]'
 
 
 
 export default function UpcomingAppointments ()
 {
+
+  const data = [
+    { id: 0, message: '15.03 (14:00) - Cardiology' },
+    { id: 1, message: '29.07 (10:30) - Cardiology' }
+  ]
   return (
 
     <div className='
-      flex flex-col items-center 
+      flex flex-col items-center
       bg-blue-600 w-96 h-80
       text-white text-center text-2xl
       rounded-xl padding-10 shadow-xl'
@@ -18,26 +24,26 @@ export default function UpcomingAppointments ()
 
       <p className='
         w-11/12 text-2xl font-bold shadow-2xl
-        pb-2 pt-2 border-b-2 border-b-blue-200
+        pb-2 pt-2 border-b-2 border-b-blue-200 mb-6
         '
       >
         Upcoming Appointments
       </p>
 
-      <Link href = { Routes.APPOINTMENT(3) }>
-        <Button className = 'bg-blue-100 hover:bg-blue-50 text-black pl-5 pr-5 text-lg h-14 m-4'>
-          15.03 (14:00) - Cardiology app.
-        </Button>
-      </Link>
-
-      <Link href = { Routes.APPOINTMENT(4) }>
-        <Button className = 'bg-blue-100 hover:bg-blue-50 text-black pl-5 pr-5 text-lg h-14 m-4'>
-          29.07 (10:30) - Cardiology app.
-        </Button>
-      </Link>
+      {
+        data.map(appointment => (
+          <Button className='
+            bg-blue-100 hover:bg-blue-50
+            text-black text-lg
+              p-4 mt-4
+          '>
+            appointment.message
+          </Button>
+        ))
+      }
 
       <Link href = { Routes.APPOINTMENTS }>
-        <Button className = 'bg-blue-100 hover:bg-blue-50 text-black pl-5 pr-5 text-lg h-14 m-4'>
+        <Button className = 'bg-blue-100 hover:bg-blue-50 text-black rounded-xl pl-5 pr-5 text-lg h-14 m-4'>
           View All
         </Button>
       </Link>
