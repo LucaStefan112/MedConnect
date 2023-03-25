bcb84732dd30fa552d037bf74c613d05cf0b8156
Tudor Gălățan <tudorgalatan@gmail.com>
2023-03-25 12:15:29 +0200

    Added View All Button
    I have added the View All button.

--- components/UpcomingAppointments/UpcomingAppointments.tsx
+++ components/UpcomingAppointments/UpcomingAppointments.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import Link from 'next/link'
+import Routes from '@/utils/Routes'
 
-export default function UpcomingAppointments() {
+
+
+export default function UpcomingAppointments ()
+{
   return (
+
     <div className='
       flex flex-col items-center 
       bg-blue-600 w-96 h-80
       text-white text-center text-2xl
       rounded-xl padding-10 shadow-xl'
     >
+
       <p className='
         w-11/12 text-2xl font-bold shadow-2xl
         pb-2 pt-2 border-b-2 border-b-blue-200
@@ -15,6 +23,14 @@ export default function UpcomingAppointments() {
       >
         Upcoming Appointments
       </p>
+
+      <Link href = { Routes.ADD_APPOINTMENT }>
+        <Button className = 'main_button text-lg h-14 m-4'>
+          View All
+        </Button>
+      </Link>
+
     </div>
+
   )
 }
