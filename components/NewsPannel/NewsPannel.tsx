fd57c57154f3331e317b930d46431a4bf5d6c5d1
Tudor Gălățan <tudorgalatan@gmail.com>
2023-03-25 11:44:49 +0200

    Added Content to News Panel
    I have added content to the News panel.

--- components/NewsPannel/NewsPannel.tsx
+++ components/NewsPannel/NewsPannel.tsx
@@ -16,18 +16,42 @@ export default function () {
   // }, []);
 
   return (
-    <div className='
+
+    <div className ='
       flex flex-col items-center 
       bg-blue-600 w-2/3 h-80
       text-white text-center text-2xl
       rounded-xl padding-10 shadow-xl'
     >
-      <p className='
+      
+      <p className ='
         w-11/12 font-bold text-3xl shadow-2xl
         pb-2 pt-2 border-b-2 border-b-blue-200'
       >
         News
       </p>
+
+      <ul>
+        
+        <li className = 'ml-16 mr-12 list-disc text-justify'>
+          Update 25.03.2023: You can now make appointments to our
+          new dermatologist, Doctor Vladislav Gregory.
+        </li>
+
+        <li className = 'ml-16 mr-12 list-disc text-justify'>
+          Update 21.03.2023: One year passed since your last medical
+          analyses. Make an appointment right now to be on track!
+        </li>
+
+        <li className = 'ml-16 mr-12 list-disc text-justify'>
+          Update 19.03.2023: It looks like you never had a heart control.
+          Considering your age, it is recommended to check your heart every
+          two years. Make an appointment to our cardiologist, Doctor George Michael. 
+        </li>
+
+      </ul>
+
     </div>
+
   )
 }
