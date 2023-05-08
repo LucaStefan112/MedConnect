9340f4c030056c44bafb60844ca956f721c2c47a
Achitei Marius <marius.achitei02@gmail.com>
2023-05-08 22:06:41 +0300

    pp

--- pages/analyses/index.tsx
+++ pages/analyses/index.tsx
@@ -1,81 +1,145 @@
-import SearchBar from '@/components/SearchBar/SearchBar'
-import VerticalList from '@/components/VerticalList/VerticalList'
-import { IGetAnalysesResponse } from '@/services/response.interfaces';
-import UserService, { IAnalysis } from '@/services/user.service';
-import DateTimeParser from '@/utils/DateTimeParser';
-import Routes from '@/utils/Routes';
-import { Button } from '@mui/material'
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import DownloadIcon from '@mui/icons-material/Download';
-// import cookieCutter from 'cookie-cutter'
-import { Console } from 'console';
+import SearchBar from "@/components/SearchBar/SearchBar";
+import VerticalList from "@/components/VerticalList/VerticalList";
+import { IGetAnalysesResponse } from "@/services/response.interfaces";
+import UserService, { IAnalysis } from "@/services/user.service";
+import DateTimeParser from "@/utils/DateTimeParser";
+import Routes from "@/utils/Routes";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import DownloadIcon from "@mui/icons-material/Download";
+import Cookies from "js-cookie";
 
-
-var cookie = require('cookie-cutter');
+var cookie = require("cookie-cutter");
 
 export default function Analyses() {
   const router = useRouter();
-  
+
   const [analyses, setAnalyses] = useState<IAnalysis[]>([
-    { id: '0', description: 'Analyses_set_0', type: '0', title: 'Analisis_0', date: new Date('2021-10-10') },
-    { id: '1', description: 'Analyses_set_1', type: '1', title: 'Analisis_1', date: new Date('2021-10-11') },
-    { id: '2', description: 'Analyses_set_2', type: '2', title: 'Analisis_2', date: new Date('2021-10-12') },
-    { id: '3', description: 'Analyses_set_3', type: '3', title: 'Analisis_3', date: new Date('2021-10-13') },
-    { id: '4', description: 'Analyses_set_4', type: '4', title: 'Analisis_4', date: new Date('2021-10-14') },
-    { id: '5', description: 'Analyses_set_5', type: '5', title: 'Analisis_5', date: new Date('2021-10-15') },
-    { id: '6', description: 'Analyses_set_6', type: '6', title: 'Analisis_6', date: new Date('2021-10-16') },
-    { id: '7', description: 'Analyses_set_7', type: '7', title: 'Analisis_7', date: new Date('2021-10-17') },
-    { id: '8', description: 'Analyses_set_8', type: '8', title: 'Analisis_8', date: new Date('2021-10-18') },
+    {
+      id: "0",
+      description: "Analyses_set_0",
+      type: "0",
+      title: "Analisis_0",
+      date: new Date("2021-10-10"),
+    },
+    {
+      id: "1",
+      description: "Analyses_set_1",
+      type: "1",
+      title: "Analisis_1",
+      date: new Date("2021-10-11"),
+    },
+    {
+      id: "2",
+      description: "Analyses_set_2",
+      type: "2",
+      title: "Analisis_2",
+      date: new Date("2021-10-12"),
+    },
+    {
+      id: "3",
+      description: "Analyses_set_3",
+      type: "3",
+      title: "Analisis_3",
+      date: new Date("2021-10-13"),
+    },
+    {
+      id: "4",
+      description: "Analyses_set_4",
+      type: "4",
+      title: "Analisis_4",
+      date: new Date("2021-10-14"),
+    },
+    {
+      id: "5",
+      description: "Analyses_set_5",
+      type: "5",
+      title: "Analisis_5",
+      date: new Date("2021-10-15"),
+    },
+    {
+      id: "6",
+      description: "Analyses_set_6",
+      type: "6",
+      title: "Analisis_6",
+      date: new Date("2021-10-16"),
+    },
+    {
+      id: "7",
+      description: "Analyses_set_7",
+      type: "7",
+      title: "Analisis_7",
+      date: new Date("2021-10-17"),
+    },
+    {
+      id: "8",
+      description: "Analyses_set_8",
+      type: "8",
+      title: "Analisis_8",
+      date: new Date("2021-10-18"),
+    },
   ]);
 
   // useEffect(() => {
   //   UserService.getAnalyses().then((res: IGetAnalysesResponse) => {
-  //     if(res.success && res.analyses){
+  //     if (res.success && res.analyses) {
   //       setAnalyses(res.analyses);
   //     } else {
   //       console.log(res.message);
   //     }
   //   });
   // }, []);
-
+  let [data, setData] = useState([]);
 
   useEffect(() => {
     async function checkAuth() {
-      const token = cookie.get('token')
-      const response = await fetch('localhost:3002/ceck-auth/${token}')
-      console.log(response)
-      console.log(token)
+      const token = Cookies.get("token");
+      // console.log(response);
+      console.log("BOMBEU");
+      console.log(token);
+      console.log("BOMBEU");
+      const response = await fetch(
+        "http://localhost:3002/check-auth/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTQ0NGNjYjg0OTgwZWI2MjVkMmZkZiIsImlhdCI6MTY4MzAwNzA0NH0.-O4Z_wSea9Q_Mk3RIaYEdlJ9dJEVyghodYvMqMXJl-A"
+      );
+      const result = await response.json();
+      // setData(response);
+      console.log(result);
+      const response2 = await fetch("http://localhost:3002/analyses", {
+        credentials: "same-origin",
+      });
+      const result2 = await response2.json();
+      console.log(result2);
     }
-    checkAuth()
-  }, [])
-
+    checkAuth();
+  }, []);
 
   return (
-
-    <div className='main_page'>
-      <div className='flex flex-row items-center justify-between w-full h-14'>
-        <SearchBar placeholder='Search analysis...' />
+    <div className="main_page">
+      <div className="flex flex-row items-center justify-between w-full h-14">
+        <SearchBar placeholder="Search analysis..." />
       </div>
-      <p className='text-4xl mt-7 mb-7 text-blue-600 w-full'>
-        My Analyses
-      </p>
-      <VerticalList className='max-h-[30.5em] hide_scroll'
-        list= {
-          analyses.map((analysis: IAnalysis) => {
-            return {
-              title:  analysis.title + ' at ' + DateTimeParser.parseDate(analysis.date),
-              onClick: () => router.push(Routes.ANALYSIS(analysis.id)),
-              icons: [
-                <a href='641946eda55feb76cc7b08c5_23-03-2023_biochemistry+haematology+microbiology.pdf' key={ 0 } download={true}>
-                  <DownloadIcon color='primary' />
-                </a>
-              ]
-            }
-          })
-        }
+      <p className="text-4xl mt-7 mb-7 text-blue-600 w-full">My Analyses</p>
+      <VerticalList
+        className="max-h-[30.5em] hide_scroll"
+        list={analyses.map((analysis: IAnalysis) => {
+          return {
+            title:
+              analysis.title + " at " + DateTimeParser.parseDate(analysis.date),
+            onClick: () => router.push(Routes.ANALYSIS(analysis.id)),
+            icons: [
+              <a
+                href="641946eda55feb76cc7b08c5_23-03-2023_biochemistry+haematology+microbiology.pdf"
+                key={0}
+                download={true}
+              >
+                <DownloadIcon color="primary" />
+              </a>,
+            ],
+          };
+        })}
       />
     </div>
-  )
+  );
 }
--- utils/Routes.ts
+++ utils/Routes.ts
@@ -1,15 +1,16 @@
 export default class Routes {
-  public static AUTH_APP = 'http://localhost:3000';
-  public static HOME = '/';
-  
-  public static REDIRECT_FROM_AUTH = '/auth-redirect/:token';
-  public static ADD_APPOINTMENT = '/appointments/add';
-  public static APPOINTMENTS = '/appointments';
+  public static AUTH_APP = "http://localhost:3000";
+  public static HOME = "/";
+
+  public static REDIRECT_FROM_AUTH = "/auth-redirect/:token";
+  public static ADD_APPOINTMENT = "/appointments/add";
+  public static APPOINTMENTS = "/appointments";
   public static APPOINTMENT = (id: any) => `/appointments/${id}`;
-  public static ANALYSES = '/analyses';
+  public static ANALYSES = "/analyses";
   public static ANALYSIS = (id: any) => `/analyses/${id}`;
-  public static MY_ACCOUNT = '/my-account';
-  public static SIGNUP = '/signup';
-  public static SIGNIN = '/signin';
-  public static LOGOUT = '/logout';
+  public static MY_ACCOUNT = "/my-account";
+  public static SIGNUP = "/signup";
+  public static SIGNIN = "/signin";
+  public static LOGOUT = "/logout";
+  public static LOGIN = "/login";
 }
