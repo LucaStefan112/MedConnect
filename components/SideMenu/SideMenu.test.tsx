27775d6234b21e2852eacc36ca077984e9addd82
Birladeanu Rares <birladeanurares@gmail.com>
2023-05-13 16:53:31 +0300

    more....test

---
+++ components/SideMenu/SideMenu.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SideMenu from './SideMenu';
+import { useRouter } from 'next/router';
+
+// mock Next.js router
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    pathname: '/',
+  }),
+}));
+
+describe('SideMenu', () => {
+  it('renders all menu buttons', () => {
+    render(<SideMenu />);
+    const homeButton = screen.getByText(/home/i);
+    const accountButton = screen.getByText(/my account/i);
+    const appointmentsButton = screen.getByText(/appointments/i);
+    const analysesButton = screen.getByText(/analyses/i);
+
+    expect(homeButton).toBeInTheDocument();
+    expect(accountButton).toBeInTheDocument();
+    expect(appointmentsButton).toBeInTheDocument();
+    expect(analysesButton).toBeInTheDocument();
+  });
+});
--- components/UserForm/UserForm.test.tsx
+++ components/UserForm/UserForm.test.tsx
@@ -1,34 +1,73 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import UserForm, { IProps } from './UserForm';
+import { render, screen, fireEvent } from '@testing-library/react';
+import UserForm from './UserForm';
+import{ IUserData, IProps } from './UserForm';
 
 describe('UserForm', () => {
-  const defaultProps: IProps = {
-    user: {
-      firstName: 'John',
-      lastName: 'Doe',
-      cnp: 1234567890,
-      birthDate: new Date('1990-01-01'),
-      sex: 'Male',
-      citizenship: 'US',
-      country: 'USA',
-      county: 'County',
-      city: 'City',
-      completeAdress: '123 Street, City, Country',
-      phoneNumber: 1234567890,
-      email: 'john@example.com',
-      maritalStatus: 'Married',
-      children: [],
-      emergencyContact: 'Jane Doe',
-    },
-    isChild: false,
+  const mockUserData: IUserData = {
+    firstName: 'John',
+    lastName: 'Doe',
+    cnp: 1234567890,
+    birthDate: new Date('1990-01-01'),
+    sex: 'Male',
+    citizenship: 'US',
+    country: 'US',
+    county: 'CA',
+    city: 'Los Angeles',
+    completeAdress: '123 Main St',
+    phoneNumber: 5551234,
+    email: 'john.doe@example.com',
+    maritalStatus: 'Single',
+    children: [],
+    emergencyContact: 'Jane Doe'
   };
 
-  test('renders UserForm component', () => {
-    render(<UserForm {...defaultProps} />);
+  const mockProps: IProps = {
+    user: mockUserData,
+    isChild: false
+  };
+
+  it('renders the user form', () => {
+    render(<UserForm {...mockProps} />);
 
     expect(screen.getByLabelText('First Name')).toBeInTheDocument();
     expect(screen.getByLabelText('Last Name')).toBeInTheDocument();
-    // Add more assertions as needed
+    expect(screen.getByLabelText('CNP')).toBeInTheDocument();
+    expect(screen.getByLabelText('Birth Date')).toBeInTheDocument();
+    expect(screen.getByLabelText('Sex')).toBeInTheDocument();
+    expect(screen.getByLabelText('Citizenship')).toBeInTheDocument();
+    expect(screen.getByLabelText('Country')).toBeInTheDocument();
+    expect(screen.getByLabelText('County')).toBeInTheDocument();
+    expect(screen.getByLabelText('City')).toBeInTheDocument();
+    expect(screen.getByLabelText('Complete Adress')).toBeInTheDocument();
+  });
+
+  it('populates the input fields with user data', () => {
+    render(<UserForm {...mockProps} />);
+
+    expect(screen.getByLabelText('First Name')).toHaveValue(mockUserData.firstName);
+    expect(screen.getByLabelText('Last Name')).toHaveValue(mockUserData.lastName);
+    expect(screen.getByLabelText('CNP')).toHaveValue(mockUserData.cnp);
+    expect(screen.getByLabelText('Birth Date')).toHaveValue('1990-01-01');
+    expect(screen.getByLabelText('Sex')).toHaveValue(mockUserData.sex);
+    expect(screen.getByLabelText('Citizenship')).toHaveValue(mockUserData.citizenship);
+    expect(screen.getByLabelText('Country')).toHaveValue(mockUserData.country);
+    expect(screen.getByLabelText('County')).toHaveValue(mockUserData.county);
+    expect(screen.getByLabelText('City')).toHaveValue(mockUserData.city);
+    expect(screen.getByLabelText('Complete Adress')).toHaveValue(mockUserData.completeAdress);
+  });
+
+  it('does not render the additional input fields when isChild is true', () => {
+    const mockChildProps: IProps = {
+      user: mockUserData,
+      isChild: true
+    };
+
+    render(<UserForm {...mockChildProps} />);
+
+    expect(screen.queryByLabelText('Phone Number')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Email')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Marital Status')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Children')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Emergency Contact')).not.toBeInTheDocument();
   });
 });
