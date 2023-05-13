13d5d14f79b9714317b6cdefeaf4ee7744522448
Birladeanu Rares <birladeanurares@gmail.com>
2023-05-13 13:58:58 +0300

    setup the coverage

--- jest.config.js
+++ jest.config.js
@@ -15,18 +15,6 @@ const customJestConfig = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: [
-    "**/*.{tsx}",
-  "!**/node_modules/**"
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
 };
